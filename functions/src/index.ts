fb5b2506d8b17a3c18fa590763e106465bccc2b4
Davi Resio <daviresio@gmail.com>
2021-04-05 22:11:21 -0300

    add firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,24 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp(functions.config().firebase);
+
+// On sign up.
+exports.processSignUp = functions.auth.user().onCreate(async (user) => {
+  const customClaims = {
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "user",
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-user-id": user.uid,
+    },
+  };
+
+  try {
+    await admin.auth().setCustomUserClaims(user.uid, customClaims);
+    // Update real-time database to notify client to force refresh.
+    const metadataRef = admin.database().ref("metadata/" + user.uid);
+    return await metadataRef.set({refreshTime: new Date().getTime()});
+  } catch (error) {
+    console.log(error);
+  }
+});
